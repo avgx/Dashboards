b265b135a7a4665fb01f53ac294c8b8f12f598a4
avgx <agovorovsky@gmail.com>
2025-10-02 16:04:58 +0300

    Example app

---
+++ Example/Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Alexey Govorovsky on 02.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
